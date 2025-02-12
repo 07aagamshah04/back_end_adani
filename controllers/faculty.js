409ea9304fd5c9e00c103e41ac5e1f88a1fd3180
07aagamshah04 <07aagamshah04@gmail.com>
2025-02-12 22:42:19 +0530

    some change in qr logic

--- controllers/faculty.js
+++ controllers/faculty.js
@@ -169,7 +169,7 @@ async function addAttendance(req, res) {
     console.log("Current Time:", currentTime);
     console.log("Time Difference (ms):", currentTime - decodedTime);
 
-    if (currentTime - decodedTime > 10000) {
+    if (currentTime - decodedTime > 600000) {
       return res.status(400).json({ message: "QR code expired." });
     }
     const faculty = await Faculty.findOne({
